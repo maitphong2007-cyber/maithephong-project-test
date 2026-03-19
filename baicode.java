09d3ee23215960eb3c8775b8f6af8712064b10d5
maitphong2007-cyber <maitphong2007@gmail.com>
2026-03-19 23:23:45 +0700

    bay gio toi da doi code

--- baicode.java
+++ baicode.java
@@ -1,5 +1,6 @@
 class baicode{
     public static void main(String[] args) {
         System.out.println("bay gio toi moi hoc git");
+        System.out.println("bay gio toi muon doi code");
     }
 }
